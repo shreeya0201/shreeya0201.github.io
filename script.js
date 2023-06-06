ed88918f90a1934ede56e673c215bb348750f518
shreeya0201 <92921913+shreeya0201@users.noreply.github.com>
2023-06-06 18:30:49 +0530

    user card 2 done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Loading,please wait");
+var data1={image : "abc.jpg",name : " Shreeya Ganji", designation:"Software Developer"};
+var data2={image : "ss.jpg",name : "Shreeya", designation:"Frontend developer"};
+var isdata1 = true;
+var displaydata;
+var changemypic = function(){
+    if(isdata1){
+        displaydata= data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("myname").innerHTML=displaydata.name;
+    document.getElementById("mydesig").innerHTML=displaydata.designation;
+}
